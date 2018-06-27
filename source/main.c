2d558f6b3920a74583d494a23ae6744f30c30310
Mr Shakos <wes.van.hamme@gmail.com>
2018-06-27 22:46:57 +0200

    Update main.c

--- source/main.c
+++ source/main.c
@@ -16,16 +16,16 @@ int _main(void) {
 
 	int sysUtil = sceKernelLoadStartModule("/system/common/lib/libSceSysUtil.sprx", 0, NULL, 0, 0, 0);
 	RESOLVE(sysUtil, sceSysUtilSendSystemNotificationWithText);
-    int gamePID = findProcess("eboot.bin");
-	procAttach(gamePID);
+    int gamePID = findProcess("eboot.bin");//first find eboot before attaching
+	procAttach(gamePID);//attach to the process
 	u64 gameCheck;
-	procReadBytes(gamePID, 0x00000, (void*)&gameCheck, sizeof(gameCheck));
+	procReadBytes(gamePID, 0x00000, (void*)&gameCheck, sizeof(gameCheck));//this reads from a random offset inside an eboot and checks the value
 
-	if (gameCheck == 0x00000) {
-	        u8 bytes[] = { 0x00};
-	        procWriteBytes(gamePID, (void*)0x000000, bytes, sizeof(bytes));
-	        sysNotify("Uncharted4 v1.00 Detected");
-	        procDetach(gamePID);
+	if (gameCheck == 0x00000) { //leets say 0xxxxffff ==  0xxxxffff = true but if its not the same = false
+	        u8 bytes[] = { 0x00};//value that needs to be replaced
+	        procWriteBytes(gamePID, (void*)0x000000, bytes, sizeof(bytes));//offset you wanne change
+	        sysNotify("Uncharted4 v1.00 Detected");//notify text
+	        procDetach(gamePID);//detach from process 
 	}
 	else if (gameCheck == 0x000000) {
 	        u8 bytes[] = { 0x00};
